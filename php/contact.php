b432fa7a4b157da38f94dfcedcdabfafc71b9782
LandonBolton <45866853+LandonBolton@users.noreply.github.com>
2018-12-17 08:42:52 -0600

    Create contact.php

---
+++ php/contact.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+  <meta name="referrer" content="origin" />
+  </head>
+<body>
+<form action="https://formspree.io/nuclearbolton@gmail.com" method="POST">
+  <input type="text" name="name">
+  <input type="email" name="_replyto">
+  <input type="submit" value="Send">
+</form>
+</body>
+</html>
